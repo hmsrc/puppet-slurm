89e1ba35397e84d09a85e742bb90445677a4f604
bhanu prasad <bhanu_prasad@hms.harvard.edu>
2017-04-06 11:59:00 -0400

    Last one didn't worked

--- manifests/node/config.pp
+++ manifests/node/config.pp
@@ -15,8 +15,8 @@ class slurm::node::config {
         group  => 'root',
         mode   => '0755',
         source => $slurm::cgroup_release_common_source_real,
-      }->
-    }
+      }
+    }->
     file { "${slurm::cgroup_release_agent_dir_real}/release_blkio":
       ensure => 'link',
       target => 'release_common',
