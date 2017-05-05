9f1641f07f1eb1762af336570b1de66a37a5c180
Jason McDonald <jason_mcdonald@hms.harvard.edu>
2017-05-05 14:08:38 -0400

    updating postrotate command for slurm 17

--- manifests/params.pp
+++ manifests/params.pp
@@ -567,7 +567,7 @@ class slurm::params {
 
   case $::osfamily {
     'RedHat': {
-      $logrotate_slurm_postrotate     = '/etc/init.d/slurm reconfig >/dev/null 2>&1'
+      $logrotate_slurm_postrotate     = '/bin/scontrol reconfig >/dev/null 2>&1'
       $logrotate_slurmdbd_postrotate  = '/etc/init.d/slurmdbd reconfig >/dev/null 2>&1'
       $logrotate_syslog_postrotate    = '/bin/kill -HUP `cat /var/run/syslogd.pid 2> /dev/null` 2> /dev/null || true'
     }
